2e9bfea98ee5520b8e704436b6cee04803c8f3a9
ivansetiawan17 <ivansetiawan170703@gmail.com>
2024-03-04 15:13:32 +0700

    selesai mengerjakan soal no 3

---
+++ ivan/operasiphp.php
@@ -0,0 +1,69 @@
+<?php
+//Soal Operator Aritmatika Mendefinisikan variabel
+$a = 15;
+$b = 5;
+
+// Menjumlahkan
+$jumlah = $a + $b;
+
+// Mengurangi
+$kurang = $a - $b;
+
+// Mengalikan
+$kali = $a * $b;
+
+// Membagi
+$bagi = $a / $b;
+
+// Sisa bagi
+$sisabagi = $a % $b;
+
+// Pangkat
+$pangkat = $a ** $b;
+
+// Menampilkan hasil nilai
+echo "Jumlah: " . $jumlah . "\n";
+echo "Selisih: " . $kurang . "\n";
+echo "Hasil perkalian: " . $kali . "\n";
+echo "Hasil bagi: " . $bagi . "\n";
+echo "Sisa bagi: " . $sisabagi . "\n";
+echo "Pangkat: " . $pangkat . "\n";
+
+//Operator Increment dan Decrement
+$a = 2;
+$y = 20;
+
+// Increment $x
+echo "Nilai a sebelum increment: " . $a . "\n";
+$a++;
+echo "Nilai a setelah increment: " . $a . "\n";
+
+// Decrement $y
+echo "Nilai b sebelum decrement: " . $y . "\n";
+$y--;
+echo "Nilai b setelah decrement: " . $y . "\n";
+
+//operasi perbandingan
+
+$x = 10;
+$y = 5;
+
+// Sama dengan
+echo "Apakah x sama dengan y? " . ($x == $y) . "\n"; 
+
+// Tidak sama dengan
+echo "Apakah x tidak sama dengan y? " . ($x != $y) . "\n"; 
+
+// Kurang dari
+echo "Apakah x kurang dari y? " . ($x < $y) . "\n"; 
+
+// Lebih dari
+echo "Apakah x  lebih dari y? " . ($x > $y) . "\n"; 
+
+// Kurang dari atau sama dengan
+echo "Apakah x  kurang dari atau sama dengan y? " . ($x <= $y) . "\n"; 
+
+// Lebih dari atau sama dengan
+echo "Apakah x  lebih dari atau sama dengan y? " . ($x >= $y) . "\n";
+
+?>
