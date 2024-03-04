9320e754fe00f365fbe28c6e95c42385111cd117
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-03-04 15:40:36 +0700

    Selesai soal No 5

---
+++ YogaFradana/OperatorAritmetika.php
@@ -0,0 +1,15 @@
+<?php
+$a = 10;
+$b = 5;
+$hasiljumlah = $a + $b;
+$hasilkurang = $a - $b;
+$hasilkali = $a * $b;
+$hasilbagi = $a / $b;
+
+echo "Hasil Perhitungannya adalah sebagai berikut : "."\n";
+
+echo "Hasil penjumlahan $a + $b = $hasiljumlah"."\n";
+echo "Hasil pengurangan $a - $b = $hasilkurang"."\n";
+echo "Hasil perkalian $a x $b = $hasilkali"."\n";
+echo "Hasil pembagian $a / $b = $hasilbagi"."\n";
+?>
