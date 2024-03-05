10486d3f4278277730838e5de9c2f8d01117c1d6
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-03-05 11:48:57 +0700

    Membuat Koneksi

---
+++ SQL/BelajarSQL.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost"; 
+$username = "root";
+$password = "";
+$database = "microdata";
+
+// Buat koneksi
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil!";
+}
+
+// $conn->close();
+?>
