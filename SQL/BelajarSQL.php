37e4d8b10583a79858e750597b7d51bda0055c3d
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-03-05 14:27:07 +0700

    Test Koneksi

--- SQL/BelajarSQL.php
+++ SQL/BelajarSQL.php
@@ -7,7 +7,33 @@ $database = "microdata";
 // Buat koneksi
 $conn = new mysqli($servername, $username, $password, $database);
 
+//query Untuk ngambil data karyawan di database microdata
+$sql = "SELECT * FROM karyawan";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    // Menampilkan data data dalam bentuk tabel
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>jabatan</th>
+                <th>Gaji</th>
+            </tr>";
+    
+    // untuk mengeluarkan data dari setiap baris perlu query sql dibawah ini
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>".$row["id"]."</td>
+                <td>".$row["nama_depan"]."</td>
+                <td>".$row["jabatan"]."</td>
+                <td>".$row["gaji"]."</td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Tidak ada data dalam tabel.";
+}
 
 // // Check koneksi
 // if ($conn->connect_error) {
