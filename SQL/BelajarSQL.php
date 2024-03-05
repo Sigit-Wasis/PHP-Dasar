b03cffdb585dd5c011a15329bdd79bfa61b090c8
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-03-05 12:31:55 +0700

    Menambahkan jawaban soal SQL dengan Format .txt

--- SQL/BelajarSQL.php
+++ SQL/BelajarSQL.php
@@ -7,12 +7,14 @@ $database = "microdata";
 // Buat koneksi
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-} else {
-    echo "Koneksi berhasil!";
-}
+
+
+// // Check koneksi
+// if ($conn->connect_error) {
+//     die("Koneksi gagal: " . $conn->connect_error);
+// } else {
+//     echo "Koneksi berhasil!";
+// }
 
 // $conn->close();
 ?>
