b0c887df2e8240f8acc171a404cf4eb9b35e92e4
dianafnioktavia23 <113124849+dianafnioktavia23@users.noreply.github.com>
2024-03-06 16:55:17 +0700

    tuggas framework laravel

---
+++ Dian/Learn_Laravel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\akun;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return "<h2> SELAMAT BELAJAR LARAVEL DI PT MICRODATA INDONESIA</h2>";
+    }
+
+   
+
+}
+
