11894fb4441921c4f4d1e828bab5551fcfec5a73
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-03-18 14:19:34 +0700

    Membuat CRUD data User

---
+++ Learn-laravel/resources/views/users/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid pt-4 px-4">
+        <h1>List of Users</h1>
+
+        <div class="mb-3">
+            <a href="{{ route('users.create') }}" class="btn btn-primary">Tambah Data</a>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                                <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
--- Learn-laravel/routes/web.php
+++ Learn-laravel/routes/web.php
@@ -14,4 +14,22 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+use App\Http\Controllers\UserController;
 
+//untuk menampilkan user
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+// Menampilkan form tambah data
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+// Menyimpan data baru
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+// Menampilkan form edit data
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+
+// Menyimpan data yang telah diubah
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+
+// Menghapus data
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
